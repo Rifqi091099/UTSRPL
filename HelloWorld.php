96cf4f668654c3f215981d765984ef89a026a817
Rifqi091099 <55833604+Rifqi091099@users.noreply.github.com>
2021-09-27 09:01:07 +0800

    Membuat HelloWorld PHP

---
+++ HelloWorld.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UTS RPL</title>
+</head>
+<body>
+    <h1>Hello World, UTS RPL 2021</h1>
+    <?php
+    echo "Rifqi";
+    echo "1810817310010";
+    echo "Hello World, UTS RPL 2021";
+    ?>
+</body>
+</html>
